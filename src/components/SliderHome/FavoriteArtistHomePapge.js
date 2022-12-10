4ecf7dcd0243f688362b2923876b35d76915dcf2
DatHip <83833987+DatHip@users.noreply.github.com>
2022-12-10 21:41:18 +0700

    -Comit arit

--- src/components/SliderHome/FavoriteArtistHomePapge.js
+++ src/components/SliderHome/FavoriteArtistHomePapge.js
@@ -1,34 +1,42 @@
-import React, { memo, useEffect, useState } from "react"
-import { useGetHomePage } from "../../api/getHomePage"
-import PlayListSelector from "../Selection/PlayListSelector"
-import FavoriteArtisItem from "../Selection/FavoriteArtisItem"
+import React, { memo, useEffect, useState } from "react";
+import { useGetHomePage } from "../../api/getHomePage";
+import PlayListSelector from "../Selection/PlayListSelector";
+import FavoriteArtisItem from "../Selection/FavoriteArtisItem";
 
 const FavoriteArtistHomePapge = () => {
-   const [datas, setData] = useState(null)
-   const { data, status } = useGetHomePage()
+  const [datas, setData] = useState(null);
+  const { data, status } = useGetHomePage();
 
-   const dataSelector = data?.data.items.find((e) => e.title === "Nghệ Sĩ Yêu Thích")
+  const dataSelector = data?.data.items.find(
+    (e) => e.title === "Nghệ Sĩ Yêu Thích"
+  );
 
-   useEffect(() => {
-      if (data) {
-         setData(dataSelector?.items)
-      }
-   }, [status])
+  useEffect(() => {
+    if (data && dataSelector) {
+      setData(dataSelector?.items);
+    }
+  }, [status]);
 
-   return (
-      <PlayListSelector title={dataSelector?.title}>
-         {datas?.length > 0 &&
-            datas.map((e, index) => {
-               let classGird = "col l-2-4 m-3 c-5"
-               if (index > 4) return
-               if (index === 4) {
-                  classGird = "col l-2-4 m-0 c-5"
-               }
+  return (
+    <PlayListSelector title={dataSelector?.title}>
+      {datas?.length > 0 &&
+        datas.map((e, index) => {
+          let classGird = "col l-2-4 m-3 c-5";
+          if (index > 4) return;
+          if (index === 4) {
+            classGird = "col l-2-4 m-0 c-5";
+          }
 
-               return <FavoriteArtisItem key={e.encodeId} clasName={classGird} item={e}></FavoriteArtisItem>
-            })}
-      </PlayListSelector>
-   )
-}
+          return (
+            <FavoriteArtisItem
+              key={e.encodeId}
+              clasName={classGird}
+              item={e}
+            ></FavoriteArtisItem>
+          );
+        })}
+    </PlayListSelector>
+  );
+};
 
-export default memo(FavoriteArtistHomePapge)
+export default memo(FavoriteArtistHomePapge);
